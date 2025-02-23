6fbab5cdb5e20eb12342d348a46a630decfd2adf
dany <mdanyprayudhaa@gmail.com>
2025-02-23 13:18:16 +0700

    fix

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,15 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
-// Buat Context
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // State untuk login
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
-// Custom hook untuk menggunakan context
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 export const useAuth = () => useContext(AuthContext);
